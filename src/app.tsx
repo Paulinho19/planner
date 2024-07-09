168775b55ee172386ea66f65427a5e458e4a1e01
Paulo <98109654+Paulinho19@users.noreply.github.com>
2024-07-09 13:05:24 -0300

    Add files via upload

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+
+export function App() {
+ 
+
+  return (
+  <div className="h-screen flex items-center justify-center">
+    <div className="max-w-3xl px-6 text-center">
+    <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+    <div className=""></div>
+    <p className="text-sm text-zinc-500">Ao planejar sua viagem pela planner você automaticamente concorda <br/>
+    com nossos <a href="#" className="text-zinc-300 underline">termos de uso</a> e <a href="#" className="text-zinc-300 underline">políticas de privacidade</a>.</p>
+  </div>
+  </div>
+  )
+}
